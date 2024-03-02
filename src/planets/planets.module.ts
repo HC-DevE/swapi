c54a685902dc0acf0dab0e33e3f1376049f87ab6
Moussa Deh <moussa.deh@my-digital-school.org>
2024-03-02 15:54:18 +0100

    planet module

--- src/planets/planets.module.ts
+++ src/planets/planets.module.ts
@@ -4,9 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtStrategy } from 'src/auth/strategies/jwt.strategy';
 import { PlanetsController } from './controllers/planets.controller';
 import { PlanetsService } from './services/planets.service';
+import { FilmsModule } from 'src/films/films.module';
+import { PeopleModule } from 'src/people/people.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Planet])],
+  imports: [TypeOrmModule.forFeature([Planet]), FilmsModule, PeopleModule],
   controllers: [PlanetsController],
   providers: [PlanetsService, JwtStrategy],
   exports: [PlanetsService],
